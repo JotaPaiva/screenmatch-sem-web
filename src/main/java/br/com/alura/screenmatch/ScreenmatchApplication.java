766e7f74f94a13ecf4769ce586c93bbd305d9ba6
JotaPaiva <jonataspaiva2002@hotmail.com>
2024-05-20 23:42:26 -0300

    Criando classe ConsumoDeApi.

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		String json = consumoAPI.obterDados("https://omdbapi.com/?t=breaking+bad&apikey=6585022c");
+		System.out.println(json);
+	}
+}
